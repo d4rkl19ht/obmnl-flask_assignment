64aa44e7b48713297e2b1d7fa6b5361f01091cb5
boying <d4rkl19ht79>
2023-12-18 06:45:51 -0500

    saving completed lab

--- app.py
+++ app.py
@@ -1,16 +1,51 @@
 # Import libraries
-
+from flask import Flask, request, url_for, redirect, render_template
 # Instantiate Flask functionality
-
+app = Flask(__name__)
 # Sample data
-
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 # Read operation
-
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions=transactions)
 # Create operation
+@app.route("/add", methods = ['GET', 'POST'])
+def add_transaction():
+    if request.method == 'POST':
+        transaction = {
+            "id" : len(transactions) + 1,
+            "date" : request.form['date'],
+            "amount" : float(request.form['amount'])
+        }
+        transactions.append(transaction)
+        return redirect(url_for("get_transactions"))
+    return render_template("form.html")
 
 # Update operation
+@app.route("/edit/<int:transaction_id>", methods = ['GET', 'POST'])
+def edit_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            if request.method == 'POST':
+                transaction['date'] = request.form['date']
+                transaction['amount'] = float(request.form['amount'])
+                return redirect(url_for("get_transactions"))
+            return render_template("edit.html", transaction = transaction)
+    return (f"id {transaction_id} not found. Please try again.")
 
 # Delete operation
+@app.route("/delete/<int:transaction_id>")
+def delete_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transactions.remove(transaction)
+            return redirect(url_for("get_transactions"))
+    return (f"id {transaction_id} not found. Please try again.")
 
 # Run the Flask app
-    
+if __name__ == "__main__":
+    app.run(debug = True)
